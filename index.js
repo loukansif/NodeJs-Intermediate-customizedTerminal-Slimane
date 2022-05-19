4e2bf820d99d6875e8033c1d9869310bea3851f2
slami <lamislimane@gmail.com>
2022-05-19 17:37:54 +0200

    add display date & hour

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
+let date;
 
+function displayDate() {
+    date = new Date();
+    console.log(date.toLocaleDateString("fr"))
+    setInterval(()=>{
+        date = new Date();
+        // console.log(date.);
+        console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)
+    },1000)
+}
+
+displayDate();
